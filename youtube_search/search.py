20b8c73e337552aae2e65ab83c8028aa2dc94116
chetanyaagrawal <chetanyaagrawal@belltech.cc>
2023-09-11 00:57:28 +0530

    search

--- youtube_search/search.py
+++ youtube_search/search.py
@@ -34,13 +34,15 @@ try:
 
 
     for i in search_response['items']:
-        video_title = i["snippet"]["title"]
-        video_id = i["id"]["videoId"]
-        json_text = json.dumps(i)
-        sql_query = "INSERT INTO videos (title, youtube_video_id, youtube_raw_response) VALUES (%s, %s, %s);"
-        # Execute the query with the provided values
-        cursor.execute(sql_query, (video_title, video_id, json_text))
-
+        try:
+            video_title = i["snippet"]["title"]
+            video_id = i["id"]["videoId"]
+            json_text = json.dumps(i)
+            sql_query = "INSERT INTO videos (title, youtube_video_id, youtube_raw_response) VALUES (%s, %s, %s);"
+            # Execute the query with the provided values
+            cursor.execute(sql_query, (video_title, video_id, json_text))
+        except Exception as e:
+            continue
     connection.commit()
 except Exception as e:
     print(e)
