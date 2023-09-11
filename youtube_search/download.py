8b99f4e0a334200b859785d66805ac92f5f19538
chetanyaagrawal <chetanyaagrawal@belltech.cc>
2023-09-12 02:21:35 +0530

    database

--- youtube_search/download.py
+++ youtube_search/download.py
@@ -2,7 +2,7 @@ from pytube import YouTube as YT
 import mysql.connector
 video_id = "8ERbrByKLBk"
 vi_url = r"https://www.youtube.com/shorts/"
-video_url = vi_url + video_id
+
 
 try:
     connection = mysql.connector.connect(
@@ -15,30 +15,29 @@ try:
     if connection.is_connected():
         print("Connected to MySQL database")
 
-    cursor = connection.cursor()
+    cursor = connection.cursor(dictionary=True)
 
     new_database = "botverse"
     cursor.execute(f"USE {new_database}")
 
     sql_query = "SELECT * FROM videos where is_downloaded_server=0"
-    result = cursor.execute(sql_query)
-    results = result.fetchall()
+    cursor.execute(sql_query)
+    results = cursor.fetchall()
+    for result in results:
+        video_id = result["youtube_video_id"]
+        video_url = vi_url + video_id
+        yt_video = YT(video_url)
+        duration_seconds = yt_video.length
+        file_name = video_id +".mp4"
 
-    yt_video = YT(video_url)
-    duration_seconds = yt_video.length
-    print(duration_seconds)
-    #check if video's duration is larger than 90 sec
-    # if duration_seconds > 90:
-    #     raise Exception('large video duration')
-    
-    #check if video already downloaded
-    # if "condition" :
-    #     raise Exception('Video already downloaded')
-    
-    strm = yt_video.streams.get_by_resolution("720p")
+        if duration_seconds > 90:
+            raise Exception('large video duration')
 
-    strm.download("videos")
-    # strm.download("videos", "filename")
+    
+        strm = yt_video.streams.get_by_resolution("720p")
+
+        # strm.download("videos")
+        strm.download("videos", file_name)
 
 except Exception as e:
     print(f"An error occurred: {str(e)}")
